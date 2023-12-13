874cbc2b21cc79f85655c5e3e8da34d1b7f28e62
dbuawork <dbuawork@gmail.com>
2023-12-13 20:36:35 +0200

    added conditions theory

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-print("Homework 'minicalc'")
-print("Names student: Dmytro Bondarenko")
-
-n1 = int(input("First number: "))
-n2 = int(input("Second number: "))
-n3 = int(input("Third number: "))
-
-result = n1 + n2 + n3
-print(f"\nResult summ : {result}")
-
-result = n1 * n2 * n3
-print(f"Result multiply : {result}")
+# Умовні оператори
+# v1
+#
